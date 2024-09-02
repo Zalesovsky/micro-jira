07001183c8b6f086e53667c8fc1e0aa895b66a60
Zalesovsky <vladislav.zalesovski@gmail.com>
2024-09-02 15:04:41 +0300

    feat: Add GlobalExceptionHandler

---
+++ user-service/src/main/java/org/example/userservice/controller/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.example.userservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.net.URI;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleAllExceptions(Exception ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        problemDetail.setTitle("An error occurred");
+        problemDetail.setDetail(ex.getMessage());
+        problemDetail.setInstance(
+                URI.create(request.getDescription(false).replace("uri=", "")));
+        return problemDetail;
+    }
+
+}
